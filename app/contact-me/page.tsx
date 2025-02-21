e245a4f1ce2b1b0dbf0fda9ed8fdfb7993205653
Muhammad Ibrahim <muhdibee@gmail.com>
2025-02-21 17:46:38 +0100

    Add Contact-me component

---
+++ app/contact-me/page.tsx
@@ -0,0 +1,5 @@
+import ContactMe from "../ui/contact-me/contact-me";
+
+export default function ContactMePage() {
+  return <ContactMe />;
+}
---
+++ app/ui/contact-me/contact-me.tsx
@@ -0,0 +1,10 @@
+import styles from "./contact-me.module.css";
+
+export default function ContactMe() {
+  return (
+    <section
+      className={`${styles.ContactMeContainer} w-full flex flex-col justify-center items-center`}>
+      <p>contact me comp</p>
+    </section>
+  );
+}
